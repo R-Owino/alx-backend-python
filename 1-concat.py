1e87e5237986946356bccb63b22038b85016dafb
R-Owino <sayeakinyi@gmail.com>
2023-10-06 14:16:02 -0700

    Type-annotated function concat

---
+++ 1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Basic annotations - concat
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Takes 2 string args and returs a concatenated string
+    '''
+    return str1 + str2
