1dd0fa2e7de0eb16c5eb769704c7eed9188d774f
R-Owino <sayeakinyi@gmail.com>
2023-10-06 15:27:45 -0700

    Type-annotated function to_kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ Complex types - string and int/float to tuple
 from typing import List, Tuple, Union
 
 
-def to_kv(k: str, v: List[Union[int, float]]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''
     Takes a string and int/float and returns a tuple
     containing both starting with the str, then a float
