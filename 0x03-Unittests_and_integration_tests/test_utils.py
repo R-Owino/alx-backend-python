11fd75fec75b1dd68ff02298288072ba3d0059c1
R-Owino <sayeakinyi@gmail.com>
2023-10-30 09:11:26 +0300

    Test utils.py methods

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,9 +4,13 @@ Unittest for utils.access_nested_map
 """
 
 from parameterized import parameterized
-from typing import Mapping, Sequence, Any
+from typing import Mapping, Sequence, Any, Dict
 import unittest
+import requests
+from unittest.mock import patch, Mock
+
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -32,3 +36,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Test access_nested_map_exception """
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ Class for testing get_json """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url: str, test_payload: Dict) -> Any:
+        """ Test get_json """
+        with patch('requests.get') as mock_get:
+            mock_get.return_value = Mock(ok=True)
+            mock_get.return_value.json.return_value = test_payload
+            self.assertEqual(get_json(test_url), test_payload)
+            mock_get.assert_called_once_with(test_url)
